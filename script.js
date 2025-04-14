6e9298f2745d98e613415b89d4d7a40abbfa2d07
Faune <119836987+FauneChan@users.noreply.github.com>
2025-04-15 02:25:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('a[href="#menu-items2"]').addEventListener('click', function (event) {
-      event.preventDefault();  // 防止默认跳转行为
-      window.scrollTo({
-        top: document.getElementById('menu-items2').offsetTop - 200,  // 导航条高度 80px
-        behavior: 'smooth'
-      });
-    });
-  });
